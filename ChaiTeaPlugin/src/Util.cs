0c12b53d571bc0d186ea37d751758c00ecd4d518
Mads Mørch Antonsen <au564064@uni.au.dk>
2023-05-30 09:53:30 +0200

    Removed logging callback as it is currently not used.

--- ChaiTeaPlugin/src/Util.cs
+++ ChaiTeaPlugin/src/Util.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 
 
 namespace ChaiTea
 {
-    public class Util 
-    {
-        public delegate void DebugCallback([MarshalAs(UnmanagedType.LPStr)]string msg, int severity);
-        [DllImport("ChaiTeaLib", EntryPoint = "Util_RegisterLogCallback")]
-        public static extern void RegisterLogCallback(DebugCallback cb);
-    }
+    //public class Util 
+    //{
+    //    public delegate void DebugCallback([MarshalAs(UnmanagedType.LPStr)]string msg, int severity);
+    //    [DllImport("ChaiTeaLib", EntryPoint = "Util_RegisterLogCallback")]
+    //    public static extern void RegisterLogCallback(DebugCallback cb);
+    //}
 }
